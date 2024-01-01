b9851bdb153ded88a5e491a9a08b67c253883885
Pawel Mitrus <pawelmitrus@pawels-mbp.home>
2024-01-01 13:04:14 +0100

    tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable tenant_id              {type = string}
+variable subscription_id        {type = string}  
+variable environment            {type = string} 
+
+variable location               {type = string}
+variable prefix                 {type = string}  
