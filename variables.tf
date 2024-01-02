97d3aa5773543a270f64ffa0fd8962f006f4bca6
Pawel Mitrus <pawelmitrus@pawels-mbp.home>
2024-01-02 20:22:53 +0100

    gitignore

--- variables.tf
+++
@@ -1,6 +0,0 @@
-variable "tenant_id" { type = string }
-variable "subscription_id" { type = string }
-variable "environment" { type = string }
-
-variable "location" { type = string }
-variable "prefix" { type = string }  
